5a990ad11fea99ffa25285d785026439d39bc2d9
HaoxuanGong <100816909+HaoxuanGong@users.noreply.github.com>
2023-11-17 21:50:11 +1300

    modify code to support uninitialized queues

--- Algorithm.py
+++ Algorithm.py
@@ -3,31 +3,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+
 def predict_disease(symptoms):
-        # Create a new dataframe with the same columns as the symptom matrix
-        user_symptoms = pd.DataFrame(columns=symptom_matrix.columns)
+    # Create a new dataframe with the same columns as the symptom matrix
+    user_symptoms = pd.DataFrame(columns=symptom_matrix.columns)
 
-        # Add the user's symptoms to the dataframe
-        for symptom in symptoms:
-            if symptom in user_symptoms.columns:
-                user_symptoms.loc[0, symptom] = \
+    # Add the user's symptoms to the dataframe
+    for symptom in symptoms:
+        if symptom in user_symptoms.columns:
+            user_symptoms.loc[0, symptom] = \
                 symptom_severity.loc[symptom_severity['Symptom'] == symptom, 'weight'].values[0]
-            else:
-                print(f'Warning: Symptom {symptom} not recognized')
-        user_symptoms.fillna(0, inplace=True)
-        # Make a prediction
-        probabilities = clf.predict_proba(user_symptoms)
+        else:
+            print(f'Warning: Symptom {symptom} not recognized')
+    user_symptoms.fillna(0, inplace=True)
+    # Make a prediction
+    probabilities = clf.predict_proba(user_symptoms)
 
-        # clf.classes_ gives you the list of classes in the order they appear in probabilities
-        classes = clf.classes_
+    # clf.classes_ gives you the list of classes in the order they appear in probabilities
+    classes = clf.classes_
 
-        # Create a dictionary that maps each class (disease) to its corresponding probability
-        disease_probability = dict(zip(classes, probabilities[0]))
+    # Create a dictionary that maps each class (disease) to its corresponding probability
+    disease_probability = dict(zip(classes, probabilities[0]))
 
-        # Filter the dictionary to show only diseases with 50% probability or higher
-        high_probability_diseases = {disease: prob for disease, prob in disease_probability.items() if prob >= 0.1}
+    # Filter the dictionary to show only diseases with 50% probability or higher
+    high_probability_diseases = {disease: prob for disease, prob in disease_probability.items() if prob >= 0}
 
-        return high_probability_diseases
+    return high_probability_diseases
+
+
 # Load the data
 symptom_severity = pd.read_csv('Symptom-severity.csv')
 dataset = pd.read_csv('dataset.csv')
@@ -39,7 +42,6 @@ symptom_severity.fillna(0, inplace=True)
 # Create a binary matrix for the symptoms
 symptom_matrix = pd.get_dummies(dataset.iloc[:, 1:].stack().reset_index(level=1, drop=True))
 symptom_matrix = symptom_matrix.groupby(level=0).sum()
-
 
 # Incorporate the weights of the symptoms
 for symptom in symptom_severity['Symptom']:
@@ -57,7 +59,7 @@ y = dataset['Disease']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train a Random Forest Classifier
-clf = RandomForestClassifier(n_estimators=5, max_depth=18,random_state=42)
+clf = RandomForestClassifier(n_estimators=5, max_depth=18, random_state=42)
 clf.fit(X_train, y_train)
 
 # Make predictions on the test set
@@ -65,5 +67,7 @@ y_pred = clf.predict(X_test)
 
 # Calculate the accuracy of the model
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
-print(predict_disease(['itching', 'nodal_skin_eruptions','shivering','chills']))
+# print(accuracy)
+print(predict_disease(['cough', 'congestion', 'acidity']))
+print(predict_disease(['cough', 'congestion', 'acidity', 'fatigue']))
+print(predict_disease(['fatigue', 'anxiety', 'runny_nose']))
--- Receive.py
+++ Receive.py
@@ -1,11 +1,34 @@
+import time
+
 import pika
 import json
+
+from pika.exceptions import AMQPConnectionError
+
 from Algorithm import predict_disease
+
 "Receive the message from RabbitMQ and provide a response to it"
+
+
 class RabbitMQReceiver:
     def __init__(self, channel, queuename):
         self.channel = channel
         self.queuename = queuename
+
+    def reconnect(self):
+        # Wait before trying to reconnect
+        time.sleep(5)  # 5 seconds delay, adjust as needed
+        # Attempt to reconnect
+        while True:
+            try:
+                # Assuming you have a method to create and return a new channel
+                self.channel = self.create_channel()
+                self.Start_receiving()
+                break
+            except (pika.exceptions.AMQPConnectionError, pika.exceptions.AMQPChannelError) as e:
+                print("Failed to reconnect to RabbitMQ:", e)
+                time.sleep(5)  # Wait for 5 seconds before trying again
+
     def Receive(self, ch, method, properties, body):
         try:
             message = body.decode('utf-8')
@@ -15,26 +38,50 @@ class RabbitMQReceiver:
         if message:
             try:
                 message = json.loads(body)
+                print(message)
                 data = self.Propose_data(message)
                 # Process the message and prepare a response
-                #Convert the response to a JSON string
+                # Convert the response to a JSON string
                 response = json.dumps(data)
-                # Construct received coorelation_id
-                prop=pika.BasicProperties(correlation_id=properties.correlation_id)
+                # Construct received correlation_id
+                prop = pika.BasicProperties(correlation_id=properties.correlation_id)
                 # Send the response back to RabbitMQ
                 routing_key = str(properties.reply_to)
                 ch.basic_publish(exchange='', routing_key=routing_key, properties=prop, body=response)
+                print(response)
+
+                ch.basic_ack(delivery_tag=method.delivery_tag)
             except json.JSONDecodeError:
                 print("Decoding JSON has failed")
+                ch.basic_nack(delivery_tag=method.delivery_tag)
         else:
             print("Message is empty")
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
     def Propose_data(self, message):
-        #Provide prodicted diseases with relevant probability
+        # Provide predicted diseases with relevant probability
         symptoms = message['symptoms']
         data = predict_disease(symptoms)
 
         return data
 
     def Start_receiving(self):
-        self.channel.basic_consume(queue=self.queuename, on_message_callback=self.Receive, auto_ack=False)
-        self.channel.start_consuming()
+        try:
+            self.channel.queue_declare(queue=self.queuename, durable=True)
+            self.channel.basic_consume(queue=self.queuename, on_message_callback=self.Receive, auto_ack=False)
+            self.channel.start_consuming()
+        except (AMQPConnectionError, pika.exceptions.AMQPChannelError) as e:
+            print("Connection lost:", e)
+            self.reconnect()
+
+    def create_connection(self):
+        """Create a new RabbitMQ connection and channel."""
+        try:
+            # Assuming that 'pika.ConnectionParameters()' is set up according to your RabbitMQ configuration
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(queue=self.queuename, durable=True)
+        except AMQPConnectionError as e:
+            print("Failed to create a RabbitMQ connection:", e)
+            time.sleep(5)  # Wait for 5 seconds before trying again
+            self.create_connection()
